cf5094b2f1aa87f68d3254843f75d24b467b2f6e
Shjani49 <shjani49@gmail.com>
2020-07-27 17:26:11 -0600

    Added switch case;implement addition,subtraction,multiplication,division;not getting output

--- src/App.js
+++ src/App.js
@@ -10,15 +10,65 @@ class App extends React.Component{
       num1: "", 
       num2: "",
       result: "" 
+      
     };
   }
+
+handlenum1 = ( event ) =>{
+    this.setState({
+      num1: event.target.value
+    })
+}
+
+handlenum2 = ( event ) =>{
+    this.setState({
+      num2: event.target.value
+    })
+}
+submit = (event) =>{
+  event.preventDefault();   
+  const value =  event.target.getAttribute('value');
+  switch (value){
+
+    case "Addition" :
+    this.setState({result : parseInt(this.state.num1) + parseInt(this.state.num2)})
+    break;
+
+    case "Subtraction" :
+      this.setState({result : parseInt(this.state.num1) - parseInt(this.state.num2)})
+    break;
+
+    case "Multiplication" :
+      this.setState({result : parseInt(this.state.num1) * parseInt(this.state.num2)})
+    break;
+    
+    case "Division" :
+      this.setState({result : parseInt(this.state.num1) / parseInt(this.state.num2)})
+    break;
+    
+    default:
+        this.setState(
+          { 
+            result : " Please select Operation"
+          })
+    break;
+    }
+
+}
+
+
+ChangeState = (event) =>{
+
+  this.setState({value: event.target.value});
+  
+}
 
 render()
 {
   return(
     <>
         <h1>Welcome to my Calculator App!</h1>
-        <form>
+        <form onSubmit = {this.submit}>  
           <label htmlFor="num1">
             Input 1:<br/>
             <input
@@ -26,12 +76,13 @@ render()
               name="num1"
               id="num1"
               required
-              value={this.state.num1} />    
+              value={this.state.num1}
+              onChange = {this.handlenum1} />    
           </label>
           <br/><br/>
           <label htmlFor = "operation">
             Operation: <br/>
-          <select>
+          <select id = "calc" value = {this.state.value} onChange ={this.ChangeState} >
               <option value = "Addition"> + </option>
               <option value = "Subtraction"> - </option>
               <option value = "Multiplication"> * </option>
@@ -46,12 +97,13 @@ render()
               name="num2"
               id="num2"
               required
-              value={this.state.num2} />    
+              value={this.state.num2} 
+              onChange = {this.handlenum2}/>    
           </label>
           <br/><br/>
-          <input type="submit" value="Calculate" />
+          <input type="submit" value="Calculate"  />
           <br/>
-          <p>Result:</p>
+          <p>Result: {this.state.result}</p>
           </form>
     </>
 
