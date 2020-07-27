80b91c25c5ed001a4fe08e9db3456523813a418c
Shjani49 <shjani49@gmail.com>
2020-07-27 15:26:06 -0600

    Added Form Elements

--- src/App.js
+++ src/App.js
@@ -2,13 +2,60 @@ import React from 'react';
 
 
 class App extends React.Component{
+  constructor ( props )
+  {
+    super( props );
+
+    this.state = {
+      num1: "", 
+      num2: "",
+      result: "" 
+    };
+  }
 
 render()
 {
   return(
-<h1> Shivani</h1>
+    <>
+        <h1>Welcome to my Calculator App!</h1>
+        <form>
+          <label htmlFor="num1">
+            Input 1:<br/>
+            <input
+              type="text"
+              name="num1"
+              id="num1"
+              required
+              value={this.state.num1} />    
+          </label>
+          <br/><br/>
+          <label htmlFor = "operation">
+            Operation: <br/>
+          <select>
+              <option value = "Addition"> + </option>
+              <option value = "Subtraction"> - </option>
+              <option value = "Multiplication"> * </option>
+              <option value = "Division"> / </option>
+          </select>
+          </label>
+          <br/><br/>
+          <label htmlFor="num2">
+            Input 2:<br/>
+            <input
+              type="text"
+              name="num2"
+              id="num2"
+              required
+              value={this.state.num2} />    
+          </label>
+          <br/><br/>
+          <input type="submit" value="Calculate" />
+          <br/>
+          <p>Result:</p>
+          </form>
+    </>
 
-  )
+  );
 }
 
 }
